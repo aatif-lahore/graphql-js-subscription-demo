d192adc8f6e7c1de19479ad4f9f75add8b36a951
Atif <you@example.com>
2018-01-01 11:48:25 +0500

    pulled code

--- src/index.js
+++ src/index.js
@@ -6,43 +6,24 @@ const bodyParser = require('body-parser');
 // This package will handle GraphQL server requests and responses
 // for you, based on your schema.
 const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
-
+const graphqlHTTP = require('express-graphql');
 const {execute, subscribe} = require('graphql');
 const {createServer} = require('http');
 const {SubscriptionServer} = require('subscriptions-transport-ws');
 
 const schema = require('./schema');
-const connectMongo = require('./mongo-connector');
-const {authenticate} = require('./authentication');
-const buildDataloaders = require('./dataloaders');
-const formatError = require('./formatError');
 
 const start = async () => {
-  const mongo = await connectMongo();
   const app = express();
 
-  const buildOptions = async (req, res) => {
-    const user = await authenticate(req, mongo.Users);
-    return {
-      // This context object is passed to all resolvers.
-      context: {
-        dataloaders: buildDataloaders(mongo),
-        mongo,
-        user,
-      },
-      formatError,
-      schema,
-    };
-  };
-  app.use('/graphql', bodyParser.json(), graphqlExpress(buildOptions));
-
+  app.use('/graphql', bodyParser.json(), graphqlHTTP({
+      schema: schema,
+      rootValue: global,
+      graphiql: true,
+    }));
   const PORT = 3000;
   app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql',
-
-    // Replace this e-mail with another to test with another user in your db.
-    passHeader: `'Authorization': 'bearer token-foo@bar.com'`,
-
     subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`,
   }));
 
